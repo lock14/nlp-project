16f10c68fd00c729f1c7ef9cacb9a7fbb8cc8b32
Jon Stites <mail+git@jonstites.com>
2017-05-06 15:35:55 -0500

    minimally working example of text -> ngrams -> combined_feature array

---
+++ to_matrix.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import argh
+import scipy
+from sklearn.feature_extraction.text import CountVectorizer
+import pathlib
+
+
+@argh.arg("--subtitles", nargs="+")
+@argh.arg("--scripts", nargs="+")
+def main(subtitles=None, scripts=None, max_ngram=3, debug=False):
+    vectorizer = CountVectorizer(
+        input="filename", ngram_range=(1, max_ngram), min_df=1, stop_words="english")
+    
+    if subtitles:
+        vectorizer.fit(subtitles)
+    if scripts:
+        vectorizer.fit(scripts)
+
+    subtitle_features = vectorizer.transform(subtitles)
+
+    script_features = vectorizer.transform(scripts)
+    features = scipy.sparse.hstack([subtitle_features, script_features]).toarray()
+    if debug:
+            print(subtitle_features.toarray())
+            print(script_features.toarray())
+            print()
+            print(features)
+
+
+if __name__ == "__main__":
+    argh.dispatch_command(main)
