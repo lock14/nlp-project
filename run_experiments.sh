2a4ea5b4635f51865932fdf19f0de24164f4baf0
Jon Stites <mail+git@jonstites.com>
2017-05-08 22:21:37 -0500

    minor script fixes

--- run_experiments.sh
+++ run_experiments.sh
@@ -6,15 +6,15 @@ movies="/home/jon/Data/nlp/project/movies-data-v1.0/metacritic+starpower+holiday
 reviews="data/reviews"
 subtitles="data/subtitles"
 
-cmd="predict.py ${movies} --seed 1 --max-features 4000"
+cmd="predict.py ${movies} --seed 1 "
 max_ngrams="1 2 3 2 3"
 min_ngrams="1 1 1 2 3"
+features="100 500 1000 2000 4000 10000"
 
-
-parallel --gnu "${cmd} --min-ngrams {1} --max-ngrams {2} {3} -t ${reviews} > trace_py/reviews_mingram_{1}_maxgram_{2}.txt" ::: $min_ngrams :::+ $max_ngrams
-parallel --gnu "${cmd} --min-ngrams {1} --max-ngrams {2} {3} -t ${subtitles} > trace_py/subtitles_mingram_{1}_maxgram_{2}.txt" ::: $min_ngrams :::+ $max_ngrams
-parallel --gnu "${cmd} --min-ngrams {1} --max-ngrams {2} {3} --use-metadata > trace_py/metadata_mingram_{1}_maxgram_{2}.txt" ::: $min_ngrams :::+ $max_ngrams
-parallel --gnu "${cmd} --min-ngrams {1} --max-ngrams {2} {3} -t ${reviews} ${subtitles} > trace_py/reviews_subtitles_mingram_{1}_maxgram_{2}.txt" ::: $min_ngrams :::+ $max_ngrams
-parallel --gnu "${cmd} --min-ngrams {1} --max-ngrams {2} {3} -t ${reviews} --use-metadata > trace_py/reviews_metadata_mingram_{1}_maxgram_{2}.txt" ::: $min_ngrams :::+ $max_ngrams
-parallel --gnu "${cmd} --min-ngrams {1} --max-ngrams {2} {3} -t ${subtitles} --use-metadata > trace_py/subtitles_metadata_mingram_{1}_maxgram_{2}.txt" ::: $min_ngrams :::+ $max_ngrams
-parallel --gnu "${cmd} --min-ngrams {1} --max-ngrams {2} {3} -t ${reviews} ${subtitles} --use-metadata > trace_py/reviews_subtitles_metadata_mingram_{1}_maxgram_{2}.txt" ::: $min_ngrams :::+ $max_ngrams
+parallel --gnu -j 1 "${cmd} --min-ngrams {1} --max-ngrams {2} -t ${reviews}                             --max-features {3} > trace_py/reviews_mingram_{1}_maxgram_{2}_max_features_{3}.txt" ::: $min_ngrams :::+ $max_ngrams ::: $features
+parallel --gnu -j 1 "${cmd} --min-ngrams {1} --max-ngrams {2} -t ${subtitles}                           --max-features {3} > trace_py/subtitles_mingram_{1}_maxgram_{2}_max_features_{3}.txt" ::: $min_ngrams :::+ $max_ngrams ::: $features
+parallel --gnu -j 1 "${cmd} --min-ngrams {1} --max-ngrams {2} --use-metadata                            --max-features {3} > trace_py/metadata_mingram_{1}_maxgram_{2}_max_features_{3}.txt" ::: $min_ngrams :::+ $max_ngrams ::: $features
+parallel --gnu -j 1 "${cmd} --min-ngrams {1} --max-ngrams {2} -t ${reviews} ${subtitles}                --max-features {3} > trace_py/reviews_subtitles_mingram_{1}_maxgram_{2}_max_features_{3}.txt" ::: $min_ngrams :::+ $max_ngrams ::: $features
+parallel --gnu -j 1 "${cmd} --min-ngrams {1} --max-ngrams {2} -t ${reviews} --use-metadata              --max-features {3} > trace_py/reviews_metadata_mingram_{1}_maxgram_{2}_max_features_{3}.txt" ::: $min_ngrams :::+ $max_ngrams ::: $features
+parallel --gnu -j 1 "${cmd} --min-ngrams {1} --max-ngrams {2} -t ${subtitles} --use-metadata            --max-features {3} > trace_py/subtitles_metadata_mingram_{1}_maxgram_{2}_max_features_{3}.txt" ::: $min_ngrams :::+ $max_ngrams ::: $features
+parallel --gnu -j 1 "${cmd} --min-ngrams {1} --max-ngrams {2} -t ${reviews} ${subtitles} --use-metadata --max-features {3} > trace_py/reviews_subtitles_metadata_mingram_{1}_maxgram_{2}_max_features_{3}.txt" ::: $min_ngrams :::+ $max_ngrams ::: $features
